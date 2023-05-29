9f0d227fc3f1604ed1bcfeebce6c3c63d2b7c91d
FHS19085 <106130232+FHS19085@users.noreply.github.com>
2023-05-29 00:58:46 +0000

    most of the way through 05 Converter GUI part 3

--- Converter_GUI_V1.py
+++ Converter_GUI_V1.py
@@ -1,9 +1,10 @@
 from tkinter import *
 
+
 class Converter:
 
 	def __init__(self):
-		
+
 		button_font = ("Arial", "12", "bold")
 		button_fg = "#FFFFFF"
 
@@ -26,49 +27,51 @@ class Converter:
 		                               justify="left")
 
 		self.temp_instructions.grid(row=1)
-		self.temp_entry = Entry(self.temp_frame,
-													 font=("Arial", "14"))
+		self.temp_entry = Entry(self.temp_frame, font=("Arial", "14"))
 
 		self.temp_entry.grid(row=2, padx=10, pady=10)
 
-
 		error = "Please enter a valid number"
-		self.temp_error = Label(self.temp_frame, text=error,
-													 fg="#9C0000")
+		self.temp_error = Label(self.temp_frame, text=error, fg="#9C0000")
 		self.temp_error.grid(row=3)
-
 
 		self.button_frame = Frame(self.temp_frame)
 		self.button_frame.grid(row=4)
 
 		self.to_celcius_button = Button(self.button_frame,
-																	 text="To Degrees C",
-																	 bg="#FF66FF",
-																	 fg=button_fg, 
-																	 font=button_font, width=12)
+		                                text="To Degrees C",
+		                                bg="#FF66FF",
+		                                fg=button_fg,
+		                                font=button_font,
+		                                width=12)
 		self.to_celcius_button.grid(row=0, column=0, padx=5, pady=5)
 
 		self.to_farenheit_button = Button(self.button_frame,
-																	 text="To Farenheit",
-																	 bg="#FF66B3",
-																	 fg=button_fg, 
-																	 font=button_font, width=12)
+		                                  text="To Farenheit",
+		                                  bg="#FF66B3",
+		                                  fg=button_fg,
+		                                  font=button_font,
+		                                  width=12)
 		self.to_farenheit_button.grid(row=0, column=1, padx=5, pady=5)
 
 		self.to_help_button = Button(self.button_frame,
-																	 text="Help / Info",
-																	 bg="#FF66B3",
-																	 fg=button_fg, 
-																	 font=button_font, width=12)
+		                             text="Help / Info",
+		                             bg="#FF66B3",
+		                             fg=button_fg,
+		                             font=button_font,
+		                             width=12)
 		self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
-		
+
 		self.to_history_button = Button(self.button_frame,
-																	 text="History / Export",
-																	 bg="#FF66FF",
-																	 fg=button_fg, 
-																	 font=button_font, width=12,
-																	 state=DISABLED)
+		                                text="History / Export",
+		                                bg="#FF66FF",
+		                                fg=button_fg,
+		                                font=button_font,
+		                                width=12,
+		                                state=DISABLED)
 		self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
+
+
 # Main Routine
 if __name__ == "__main__":
 	root = Tk()
